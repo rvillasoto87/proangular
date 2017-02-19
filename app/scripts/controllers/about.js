e4eb609eb4976a8eb00bdc16f946f22eb0e975c0
Arletty Concepcion <arle_rauli@Arlettys-iMac.local>
2017-02-19 08:55:55 -0500

    first

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name proangularApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the proangularApp
+ */
+angular.module('proangularApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name proangularApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the proangularApp
+ */
+angular.module('proangularApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
